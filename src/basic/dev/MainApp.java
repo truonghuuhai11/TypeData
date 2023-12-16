dbb2c1002fbe3108233a6a6cd5e13f4e3b194719
Trương Hữu Hải <144077948+truonghuuhai11@users.noreply.github.com>
2023-12-16 07:41:36 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,38 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hovaten = "Nguyen Van A";
+		int maso = 123;
+		int so = 1234567890;
+		double diemtb = 7.5;
+		String tentruong = "Hue IC";
+		String tenlop = "23CDTH41";
+		String tenhang = "Honda";
+		String giatien = "1.000.000.000 VND";
+		String mauson = "NH830M";
+		String diachi = "12 Tran Van Ky, Tay Loc, Hue";
+		int sdt = 123456789;
+		double hesoluong = 1.8;
+		System.out.println("Thong tin mot sinh vien");
+		System.out.println(" • ten: " + hovaten);
+		System.out.println(" • ma sinh vien: " + maso);
+		System.out.println(" • chung minh nhan dan: " + so);
+		System.out.println(" • ten lop: " + tenlop);
+		System.out.println(" • ten truong: " + tentruong);
+		System.out.println(" • diem trung binh: " + diemtb);
+		System.out.println("Thong tin cong nhan");
+		System.out.println(" • ten: " + hovaten);
+		System.out.println(" • dia chi: " + diachi);
+		System.out.println(" • so dien thoai: " + sdt);
+		System.out.println(" • he so luong: " + hesoluong);
+		System.out.println("Thong tin mot chiec xe car");
+		System.out.println(" • ten: " + hovaten);
+		System.out.println(" • hang: " + tenhang);
+		System.out.println(" • gia: " + giatien);
+		System.out.println(" • mau son: " + mauson);
+	}
+
+}
